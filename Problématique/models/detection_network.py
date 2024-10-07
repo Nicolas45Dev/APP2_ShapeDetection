6322bb65801975cfce07323a7915724a72802609
nicolas <nico645@outlook.com>
2024-10-07 10:29:08 -0400

    Sweet Pain

--- Problématique/models/detection_network.py
+++ Problématique/models/detection_network.py
@@ -8,37 +8,37 @@ class DetectionModel(torch.nn.Module):
         super(DetectionModel, self).__init__()
         self.num_classes = num_classes
         # Convolutions pour extraire les caractéristiques
-        self.feature_extractor = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1),  # Change input channel to 1 for grayscale
+        self.feature_extractor = nn.Sequential(# 1x53x53
+            nn.Conv2d(1, 8, 6, padding=0),# 8x48x48
             nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(16, 64, 3, padding=1),
+            nn.MaxPool2d(2, 2),# 16x24x24
+            nn.Conv2d(8, 32, 3, padding=1),# 32x24x24
             nn.LeakyReLU(0.1),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 96, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, 2),# 64x12x12
+            nn.Conv2d(32, 48, 3, padding=1),# 48x12x12
             nn.LeakyReLU(0.1),
-            nn.Conv2d(96, 64, 3),
+            nn.Conv2d(48, 32, 3, padding=1),# 32x12x12
             nn.LeakyReLU(0.1),
-            nn.Conv2d(64, 96, 3, padding=1),
+            nn.Conv2d(32, 48, 3, padding=1),# 48x12x12
             nn.LeakyReLU(0.1),
-            nn.BatchNorm2d(96),
-            nn.Conv2d(96, 96, 3, padding=1),
+            nn.BatchNorm2d(48),
+            nn.Conv2d(48, 48, 3, padding=1),# 48x12x12
             nn.LeakyReLU(0.1),
-            nn.Conv2d(96, 64, 4, padding=2),
+            nn.Conv2d(48, 32, 5, padding=2),# 32x12x12
             nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2)
+            nn.MaxPool2d(2, 2)# 32x6x6
         )
 
         # Couche pour prédire l'existence, la position et la taille des boîtes englobantes
         # On prédit une boîte par cellule
-        self.prediction_layer = nn.Sequential(
-            nn.Conv2d(64, 72, 3, padding=1),
+        self.prediction_layer = nn.Sequential(# 64x6x6
+            nn.Conv2d(32, 36, 3, padding=1),# 72x6x6
             nn.LeakyReLU(0.1),
-            nn.BatchNorm2d(72),
-            nn.Conv2d(72, 147, 6, padding=0),
+            nn.BatchNorm2d(36),
+            nn.Conv2d(36, 3 * 7 * 7, 6, padding=0),# 147x1x1
             nn.LeakyReLU(0.1),
-            nn.Conv2d(147, 147, 3, padding=1),
+            nn.Conv2d(3 * 7 * 7, 3 * 7 * 7, 3, padding=1),# 147x1x1 linéaire!
         )# nn.Conv2d(128, 3 * 7, 4, padding=2)
 
 
