3774fa77c0724400d01e02918d6737d8eea2ae17
Nicolas <nico645@outlook.com>
2024-10-07 15:26:57 -0400

    Cancer #1

--- Problématique/models/Localisation_Loss.py
+++ Problématique/models/Localisation_Loss.py
@@ -1,8 +1,8 @@
-import numpy as np
 import torch
 import torch.nn as nn
-from sympy.physics.units import length
-from tensorflow.dtensor.python.config import is_gpu_present
+from scipy.optimize import linear_sum_assignment
+from torch import classes
+from torch.nn.functional import mse_loss
 
 
 class LocalizationLoss(nn.Module):
@@ -17,16 +17,49 @@ class LocalizationLoss(nn.Module):
         self.CrossEntropyLoss = nn.CrossEntropyLoss()
 
     def forward(self, output, target):
-        # Output se compose de 4 colonnes : x, y, w, h, une prédiction de la boite dans l'image (N, 4)
-        # Target se compose de 5 colonnes : x, y, w, h, c pour chaque boîte de forme (N, 3, 5)
-        # Somme la différence entre les coordonnées prédites et les coordonnées réelles
 
-        # Calcul de la perte pour la présente d'un carré
-        presence_loss = self.BCELoss(output[:,:, 0], target[:,:, 0])
-        # Calcul de la perte de localisation
-        MSEloss =  self.MSELoss(output[:,:,:4], target[:,:,:4])
-        # Calcul de la perte de classification
-        class_loss = self.CrossEntropyLoss(output[:,:,4:], target[:,:,4].long())
+        # presence_loss = torch.zeros([target.size(0),target.size(1),output.size(1)])
+        # mse_loss = torch.zeros([target.size(0),target.size(1),output.size(1)])
+        # class_loss = torch.zeros([target.size(0),target.size(1),output.size(1)])
+        # for i in range(target.size(1)):
+        #     for j in range(output.size(1)):
+        #         # Calcul de la perte pour la présente d'un carré
+        #         presence_loss[:,i,j] = self.BCELoss(output[:,j, 0], target[:,i, 0])
+        #         # Calcul de la perte de localisation
+        #         mse_loss[:,i,j] =  self.MSELoss(output[:,j,1:4], target[:,i,1:4])
+        #         # Calcul de la perte de classification
+        #         class_loss[:,i,j] = self.CrossEntropyLoss(output[:,j,4:], target[:,i,4].long())
+        # presence_loss_index = torch.argmin(presence_loss,dim=2)
+        # mse_loss_index = torch.argmin(mse_loss,dim=2)
+        # class_loss_index = torch.argmin(class_loss,dim=2)
+        #
+        # return  (self._alpha * presence_loss_min) + (self._beta * mse_loss_min) + (self._gamma * class_loss_min)
+        # #return (self._alpha * presence_loss) + (self._beta * MSEloss) + (self._gamma * class_loss)
 
-        return  (self._alpha * presence_loss) + (self._beta * MSEloss) + (self._gamma * class_loss)
-        #return (self._alpha * presence_loss) + (self._beta * MSEloss) + (self._gamma * class_loss)
+        loss = 0
+
+        for i in range(target.size(0)):
+
+            predictions = output[i]
+            targets = target[i]
+
+            box_predictions = predictions[:, 1:4]
+            box_targets = targets[:, 1:4]
+
+            distance = torch.cdist(box_predictions, box_targets, p=2)
+
+            row, col = linear_sum_assignment(distance.detach().cpu().numpy())
+
+            for j,k in zip(row, col):
+                mse_loss = self.MSELoss(box_predictions[j], box_targets[k])
+
+                presence_loss = self.BCELoss(predictions[k, 0], targets[j, 0])
+
+                class_prob = predictions[j, 4:]
+                class_target = targets[k, 4].long()
+                classification_loss = self.CrossEntropyLoss(class_prob.unsqueeze(0), class_target.unsqueeze(0))
+
+                loss += (self._alpha * presence_loss) + (self._beta * mse_loss) + (self._gamma * classification_loss)
+
+        return loss / target.size(0)
+
